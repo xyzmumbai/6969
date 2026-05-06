34e9a8902560209f1a7ced71015df52af7986b9b
KARAN-ANARASE <karananarase@gmail.com>
2026-05-07 00:18:13 +0530

    by khem

---
+++ kr/khem/java-msg/main.java
@@ -0,0 +1,7 @@
+// Main.java
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello from the Java container!");
+        System.out.println("The JVM is up and running.");
+    }
+}
---
+++ kr/khem/shell-script/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "today's date==$(date)"
+
